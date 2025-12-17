7557f290ad47d3523d8ea11b5be6ccc2c43b7ca1
Sergei Akimov <blackgold44441@gmail.com>
2025-12-17 08:24:55 +0300

    refactor: move games logic to games package

---
+++ brain_games/games/calc.py
@@ -0,0 +1,26 @@
+import random
+
+
+RULE_TEXT = 'What is the result of the expression?'
+OPERATIONS = ['+', '-', '*']
+
+
+def calculate_operation(num1: int, num2: int, op: str) -> int:
+    match op:
+        case "+":
+            return num1 + num2
+        case "-":
+            return num1 - num2
+        case "*":
+            return num1 * num2
+        case _:
+            raise ValueError(f"Unknown operation: {op}")
+
+
+def get_round() -> tuple[str, str]:
+    num1 = random.randint(1, 100)
+    num2 = random.randint(1, 100)
+    op = random.choice(OPERATIONS)
+    question = f'{num1} {op} {num2}'
+    correct_answer = str(calculate_operation(num1, num2, op))
+    return question, correct_answer
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,19 +1,5 @@
-import random
+from brain_games.games.even import RULE_TEXT, get_round
 from brain_games.engine import run_game
-
-
-RULE_TEXT = 'Answer "yes" if the number is even, otherwise answer "no".'
-
-
-def is_even(number: int) -> bool:
-    return number % 2 == 0
-
-
-def get_round() -> tuple[str, str]:
-    number = random.randint(1, 100)
-    question = str(number)
-    correct_answer = "yes" if is_even(number) else "no"
-    return question, correct_answer
 
 
 def main() -> None:
