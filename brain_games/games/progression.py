5f4ca2b4640ca9b51e4b1c83cf2c4c67d6d369bb
Sergei Akimov <blackgold44441@gmail.com>
2025-12-21 20:32:48 +0300

    feat: implement game "Arithmetic Progression"

---
+++ brain_games/games/progression.py
@@ -0,0 +1,30 @@
+import random
+
+
+RULE_TEXT = 'What number is missing in the progression?'
+
+
+def generate_progression() -> list:
+    start = random.randint(1, 100)
+    length = random.randint(5, 10)
+    step = random.randint(1, 5)
+    progression = []
+    for i in range(length):
+        progression.append(start)
+        start = start + step
+    return progression
+
+
+def get_round() -> tuple[str, str]:
+    current_progression = generate_progression()
+
+    index = random.randint(0, len(current_progression)-1)
+    correct_answer = str(current_progression[index])
+
+    current_progression[index] = '..' # Заменили число на точки
+
+    progression_strs = []
+    for item in current_progression:
+        progression_strs.append(str(item))
+        question = ' '.join(progression_strs)
+    return question, correct_answer
