8cdb8f931830a52d1a598453f61ea1cd643b26b5
Sergei Akimov <blackgold44441@gmail.com>
2025-12-22 18:34:44 +0300

    feat: implement game "is number prime?"

---
+++ brain_games/games/prime.py
@@ -0,0 +1,28 @@
+import random
+
+
+RULE_TEXT = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(number: int) -> bool:
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False
+    
+    i = 3
+    while i * i <= number:
+        if number % i == 0:
+            return False
+        i += 2
+
+    return True
+                
+
+def get_round() -> tuple[str, str]:
+    number = random.randint(1, 100)
+    question = f'{number}'
+    correct_answer = "yes" if is_prime(number) else "no"
+    return question, correct_answer
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+from brain_games.games.prime import RULE_TEXT, get_round
+from brain_games.engine import run_game
+
+
+def main() -> None:
+    run_game(RULE_TEXT, get_round)
+
+
+if __name__ == "__main__":
+    main()
